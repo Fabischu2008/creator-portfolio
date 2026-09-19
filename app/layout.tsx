d19a58ca73b39c6addffef634db240500c46075e
Fabischu2008 <122690050+Fabischu2008@users.noreply.github.com>
2026-09-19 02:07:21 +0200

    stark

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,18 +35,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/icon.svg", type: "image/svg+xml" },
     ],
     apple: "/apple-icon.png",
   },
